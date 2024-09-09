633dd15d3cf945967069eeb1707c014dd7974c45
Ricgo01 <128996354+Ricgo01@users.noreply.github.com>
2024-09-09 10:12:18 -0600

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,78 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int num;
+int *FibVals; // Arreglo para almacenar los valores de Fibonacci
+
+void *Cfibonacci(void *paramID){
+    int *n = (int*)paramID;
+
+    // Tener predeterminado los primeros valores de Fibonacci
+    if (*n == 0){
+        FibVals[*n] = 0;
+    }
+    else if (*n == 1){
+        FibVals[*n] = 1;
+    }
+    // Cuando el n es > 1 
+    else{
+        int a = 0, b = 1;
+        for (int i = 2; i <= *n; i++){
+            int valor = a + b;
+            // a y b se van cambiando de posicion
+            a = b;
+            b = valor;
+        }
+        FibVals[*n] = b;
+    }
+    pthread_exit(NULL);
+}
+
+int main (int argc, char *argv[]){
+
+    printf("Ingresa el numero que quieras calcular para calcular la serie de Fibonacci: ");
+    scanf("%d", &num);
+
+    // Validacion del rango de num 
+    if (num < 0 || num > 100){
+        printf("Tu numero se encuentra fuera del rango permitido\n");
+        return 1;
+    }
+
+    // Arreglo para almacenar los valores de Fibonacci
+    FibVals = (int *)malloc((num+1) * sizeof(int));
+
+    pthread_t threadsID[num+1];
+    int param[num+1];
+
+    int rc;
+    // Cada hilo esta creando una parte del fibonacci
+    for (int i = 0; i <= num; i++){
+        param[i] = i;
+        rc = pthread_create(&threadsID[i], NULL, Cfibonacci, (void*)&param[i]);
+
+        if (rc){
+            printf("ERROR: No se pudo crear el hilo %d\n", rc);
+            exit(-1);
+        }
+        // Esperar a que el hilo termine
+        pthread_join(threadsID[i], NULL);
+        printf("F(%d) = %d\n", i, FibVals[i]);
+    }
+
+    // Luego de tener todos los resultados, se suman
+    long long sum = 0;
+    for (int i = 0; i <= num; i++){
+        sum += FibVals[i];
+    }
+
+    printf("La suma total de los numeros de Fibonacci hasta %d es: %lld\n", num, sum);
+
+    // Liberar la memoria reservada
+    free(FibVals);
+
+    return 0;
+}
+
+
---
+++ p2B.c
@@ -0,0 +1,46 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_THreads 10
+
+void *Printhello(void *paramID) {
+    // cast de puntero a void hacia puntero a int
+    int *id; //puntero a entero
+    id = (int*)paramID;
+    printf("Hola Mundo hilo #%d\n", *id);
+    pthread_exit(NULL);
+    return(NULL);
+}
+
+int main(int arge, char*argv[]){
+    //parametro 1, usar ID para identificar cada hilo de manera segura
+    pthread_t threadsID[NUM_THreads];
+    pthread_attr_t attr;
+    pthread_attr_init (&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+
+    //parametro a compartir equivalente al num de hilo
+    //parametro 4, puntero a void
+    int rc, t, param [NUM_THreads];
+
+    for(t = 0; t < NUM_THreads; t++){
+        param [t] = t;
+        //Creamos al hilo (id, atrr, subrutina, valo a operar param de paso)
+        rc = pthread_create(&threadsID[t],NULL, Printhello, (void *)&param[t]);
+
+        //verificar si ocurrio error al crear algun hilo
+
+        if (rc){
+            printf("ERROR VA PAPA NO TUVISTE 0 EN %d\n", rc);
+            exit(-1);
+        }
+
+        pthread_join(threadsID[t], NULL);
+        
+    }
+
+    pthread_attr_destroy(&attr);
+    pthread_exit(NULL);	
+}
